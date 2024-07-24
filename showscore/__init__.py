4fa7560cc2bd572353f5745b6c5883ddbbdfaca6
Sven <svenh91@gmail.com>
2024-07-24 20:34:04 +0100

    0.1.3

--- showscore/__init__.py
+++ showscore/__init__.py
@@ -3,7 +3,7 @@ from .utils import getUniqueDivId, runningInNotebook
 from .html_renderer import makeHTML, musicXMLToScript, showXML, backend
 from .show import show
 
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Sven Hollowell"
 __license__ = "MIT"
 __url__ = "https://github.com/supersational/showscore"
